5c3866d302a910809306468e68bfa80df5943e95
JorgeLoaiza98 <jorge_loaiza23161@elpoli.edu.co>
2021-09-13 23:04:58 -0500

    13/09/2021 11

--- service-cinema/src/main/java/co/com/poli/cinema/CinemaApplication.java
+++ service-cinema/src/main/java/co/com/poli/cinema/CinemaApplication.java
@@ -2,10 +2,9 @@ package co.com.poli.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-
-
+@EnableEurekaClient
 @SpringBootApplication
 public class CinemaApplication {
 
